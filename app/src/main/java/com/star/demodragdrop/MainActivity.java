b1347a3da5df19c8bcbf9045a43ec428b8b25222
star <s0913940588@gmail.com>
2017-11-14 16:09:49 +0800

    YA

--- app/src/main/java/com/star/demodragdrop/MainActivity.java
+++ app/src/main/java/com/star/demodragdrop/MainActivity.java
@@ -17,9 +17,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     View.OnTouchListener listener = new View.OnTouchListener() {
+        private  float x,y;
+        private int mx ,my;
         @Override
         public boolean onTouch(View v, MotionEvent event) {
-            return false;
+            switch ((event.getAction()))
+            {
+                case MotionEvent.ACTION_DOWN:   //使用者按下
+                    x = event.getX();
+                    y = event.getY();
+                case MotionEvent.ACTION_MOVE:   //使用者移動
+                    mx = (int) (event.getRawX() -x);
+                    my = (int) (event.getRawY() - getContentViewTop() - y);
+                    v.layout(mx , my , mx + v.getWidth() , my + v.getHeight());
+                    break;
+            }
+            return true;
         }
     };
+    //計算ContentView的高度計算
+    public int getContentViewTop() {
+        View contentView = getWindow().findViewById(android.R.id.content);
+        int[] coordinates = new int[2];
+        contentView.getLocationOnScreen(coordinates);
+        return coordinates[1];
+    }
 }
